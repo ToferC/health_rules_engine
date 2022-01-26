28d7c80093f503bccaedf5976a9d6cb19b79388a
toferc <cgeist7@gmail.com>
2022-01-26 16:41:38 -0500

    add update to models

--- src/models/person.rs
+++ src/models/person.rs
@@ -18,7 +18,7 @@ use crate::get_country_by_id;
 
 use super::PublicHealthProfile;
 
-#[derive(Debug, Clone, Deserialize, Serialize, Queryable, Insertable)]
+#[derive(Debug, Clone, Deserialize, Serialize, Queryable, Insertable, AsChangeset)]
 #[table_name = "persons"]
 /// Referenced by PublicHealthProfile
 /// Referenced by Trip
@@ -40,6 +40,128 @@ pub struct Person {
     pub approved_access_level: String, // AccessLevel
     pub approved_access_granularity: String, // Granularity
     pub created_at: NaiveDateTime,
+}
+
+
+// Non Graphql
+impl Person {
+    pub fn create(conn: &PgConnection, person: &NewPerson) -> FieldResult<Person> {
+        let res = diesel::insert_into(persons::table)
+        .values(person)
+        .get_result(conn);
+        
+        graphql_translate(res)
+    }
+    
+    pub fn get_or_create(conn: &PgConnection, person: &NewPerson) -> FieldResult<Person> {
+        let res = persons::table
+        .filter(persons::travel_document_id.eq(&person.travel_document_id))
+        .filter(persons::family_name.eq(&person.family_name))
+        .filter(persons::travel_document_issuer_id.eq(&person.travel_document_issuer_id))
+        .filter(persons::birth_date.eq(&person.birth_date))
+        .distinct()
+        .first(conn);
+        
+        let person = match res {
+            Ok(p) => p,
+            Err(e) => {
+                // Person not found
+                println!("{:?}", e);
+                let p = Person::create(conn, person).expect("Unable to create person");
+                p
+            }
+        };
+        Ok(person)
+    }
+    
+    pub fn update(&self, conn: &PgConnection) -> FieldResult<Self> {
+        let res = diesel::update(persons::table)
+        .filter(persons::id.eq(&self.id))
+        .set(self)
+        .get_result(conn)?;
+        
+        Ok(res)
+    }
+}
+
+#[Object]
+impl Person {
+    
+    #[graphql(
+        guard = "RoleGuard::new(Role::Analyst)",
+        visible = "is_analyst",
+    )]
+    pub async fn family_name(&self) -> FieldResult<String> {
+        Ok(self.family_name.to_owned())
+    }
+    
+    #[graphql(
+        guard = "RoleGuard::new(Role::Analyst)",
+        visible = "is_analyst",
+    )]
+    pub async fn given_name(&self) -> FieldResult<String> {
+        Ok(self.given_name.to_owned())
+    }
+    
+    #[graphql(
+        guard = "RoleGuard::new(Role::Analyst)",
+        visible = "is_analyst",
+    )]
+    pub async fn additional_names(&self) -> FieldResult<Option<Vec<String>>> {
+        Ok(self.additional_names.to_owned())
+    }
+    
+    pub async fn birth_date(&self) -> FieldResult<String> {
+        Ok(self.birth_date.format("%Y-%m-%d").to_string())
+    }
+    
+    pub async fn approved_access_level(&self) -> FieldResult<String> {
+        Ok(self.approved_access_level.to_owned())
+    }
+    
+    pub async fn approved_access_granularity(&self) -> FieldResult<String> {
+        Ok(self.approved_access_granularity.to_owned())
+    }
+    
+    pub async fn travel_document_issuer(&self, context: &Context<'_>) -> FieldResult<Country> {
+        get_country_by_id(context, self.travel_document_issuer_id)
+    }
+    
+    #[graphql(
+        guard = "RoleGuard::new(Role::Analyst)",
+        visible = "is_analyst",
+    )]
+    /// This is personally identifiable information and can only be accessed
+    /// by Analyst or Admin roles.
+    pub async fn travel_document_id(&self) -> FieldResult<String> {
+        Ok(self.travel_document_id.to_owned())
+    }
+    
+    pub async fn public_health_profile(&self, context: &Context<'_>) -> FieldResult<PublicHealthProfile> {
+        let conn = get_connection_from_context(context);
+        
+        let res = public_health_profiles::table
+        .filter(public_health_profiles::person_id.eq(self.id))
+        .first(&conn);
+        
+        graphql_translate(res)
+    }
+    
+    pub async fn trips(&self, context: &Context<'_>) -> FieldResult<Vec<Trip>> {
+        let conn = get_connection_from_context(context);
+        
+        let res = trips::table.
+        filter(trips::person_id.eq(self.id))
+        .order_by(trips::arrival_time)
+        .order_by(trips::person_id)
+        .load::<Trip>(&conn);
+        
+        graphql_translate(res)
+    }
+    
+    pub async fn created_at(&self) -> NaiveDateTime {
+        self.created_at
+    }
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize, Insertable, SimpleObject)]
@@ -113,116 +235,3 @@ impl NewPerson {
         }
     }
 }
-
-// Non Graphql
-impl Person {
-    pub fn create(conn: &PgConnection, person: &NewPerson) -> FieldResult<Person> {
-        let res = diesel::insert_into(persons::table)
-            .values(person)
-            .get_result(conn);
-
-        graphql_translate(res)
-    }
-
-    pub fn get_or_create(conn: &PgConnection, person: &NewPerson) -> FieldResult<Person> {
-        let res = persons::table
-            .filter(persons::travel_document_id.eq(&person.travel_document_id))
-            .filter(persons::family_name.eq(&person.family_name))
-            .filter(persons::travel_document_issuer_id.eq(&person.travel_document_issuer_id))
-            .filter(persons::birth_date.eq(&person.birth_date))
-            .distinct()
-            .first(conn);
-
-        let person = match res {
-            Ok(p) => p,
-            Err(e) => {
-                // Person not found
-                println!("{:?}", e);
-                let p = Person::create(conn, person).expect("Unable to create person");
-                p
-            }
-        };
-        Ok(person)
-    }
-}
-
-#[Object]
-impl Person {
-
-    #[graphql(
-        guard = "RoleGuard::new(Role::Analyst)",
-        visible = "is_analyst",
-    )]
-    pub async fn family_name(&self) -> FieldResult<String> {
-        Ok(self.family_name.to_owned())
-    }
-
-    #[graphql(
-        guard = "RoleGuard::new(Role::Analyst)",
-        visible = "is_analyst",
-    )]
-    pub async fn given_name(&self) -> FieldResult<String> {
-        Ok(self.given_name.to_owned())
-    }
-
-    #[graphql(
-        guard = "RoleGuard::new(Role::Analyst)",
-        visible = "is_analyst",
-    )]
-    pub async fn additional_names(&self) -> FieldResult<Option<Vec<String>>> {
-        Ok(self.additional_names.to_owned())
-    }
-
-    pub async fn birth_date(&self) -> FieldResult<String> {
-        Ok(self.birth_date.format("%Y-%m-%d").to_string())
-    }
-
-    pub async fn approved_access_level(&self) -> FieldResult<String> {
-        Ok(self.approved_access_level.to_owned())
-    }
-
-    pub async fn approved_access_granularity(&self) -> FieldResult<String> {
-        Ok(self.approved_access_granularity.to_owned())
-    }
-
-    pub async fn travel_document_issuer(&self, context: &Context<'_>) -> FieldResult<Country> {
-        get_country_by_id(context, self.travel_document_issuer_id)
-    }
-
-    #[graphql(
-        guard = "RoleGuard::new(Role::Analyst)",
-        visible = "is_analyst",
-    )]
-    /// This is personally identifiable information and can only be accessed
-    /// by Analyst or Admin roles.
-    pub async fn travel_document_id(&self) -> FieldResult<String> {
-        Ok(self.travel_document_id.to_owned())
-    }
-
-    pub async fn public_health_profile(&self, context: &Context<'_>) -> FieldResult<PublicHealthProfile> {
-        let conn = get_connection_from_context(context);
-
-        let res = public_health_profiles::table
-            .filter(public_health_profiles::person_id.eq(self.id))
-            .first(&conn);
-
-        graphql_translate(res)
-    }
-
-    pub async fn trips(&self, context: &Context<'_>) -> FieldResult<Vec<Trip>> {
-        let conn = get_connection_from_context(context);
-
-        let res = trips::table.
-            filter(trips::person_id.eq(self.id))
-            .order_by(trips::arrival_time)
-            .order_by(trips::person_id)
-            .load::<Trip>(&conn);
-
-        graphql_translate(res)
-    }
-
-    pub async fn created_at(&self) -> NaiveDateTime {
-        self.created_at
-    }
-}
-
